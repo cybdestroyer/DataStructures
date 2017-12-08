28bcd6bb615aff77a05f87b5b1aa62c082e7dce3
LT <lteran@email.arizona.edu>
2017-12-07 19:50:51 -0800

    Working on the Graph data structure. Building functionality and unit tests.

---
+++ DataStructures.Tests/GraphUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using DataStructures.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Tests
+{
+    [TestClass]
+    public class GraphUnitTests
+    {
+        [TestMethod]
+        [TestCategory("GPH - Basic")]
+        public void TestMethod1()
+        {
+            var graph = new Graph(6);
+            Assert.IsTrue(graph.AddVertex("A"));
+            Assert.IsFalse(graph.AddVertex("A"));
+            Assert.IsTrue(graph.AddVertex("C"));
+            Assert.IsTrue(graph.AddVertex("G"));
+            Assert.IsTrue(graph.AddVertex("Z"));
+            Assert.IsTrue(graph.AddVertex("X"));
+            Assert.IsTrue(graph.AddVertex("B"));
+
+            Assert.IsFalse(graph.AddVertex("O"));
+            Assert.IsFalse(graph.AddVertex("B"));
+        }
+
+        [TestMethod]
+        [TestCategory("")]
+        public void TestMethod2()
+        {
+            var graph = new Graph(4);
+            Assert.IsTrue(graph.AddVertex("Start"));
+            Assert.IsTrue(graph.AddVertex("Checkpoint 1"));
+            Assert.IsTrue(graph.AddVertex("Checkpoint 2"));
+            Assert.IsTrue(graph.AddVertex("Finish"));
+
+            Assert.AreEqual(4, graph.VertexCount);
+
+            Assert.IsFalse(graph.RemoveVertex("Checkpoint 3"));
+            Assert.IsTrue(graph.RemoveVertex("Finish"));
+            Assert.AreEqual(3, graph.VertexCount);
+            Assert.IsTrue(graph.AddVertex("Checkpoint 3"));
+        }
+    }
+}
--- DataStructures/Objects/Graph.cs
+++ DataStructures/Objects/Graph.cs
@@ -1,12 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructures.Objects
 {
-    class Graph
+    public class Graph
     {
+        private int[,] matrix { get; set; }
+        private Dictionary<string, int> vertices { get; set; }
+
+        public int VertexCount { get { return vertices.Keys.Count; } }
+
+        public Graph(int size)
+        {
+            matrix = new int[size, size];
+            vertices = new Dictionary<string, int>();
+        }
+
+        public bool AddVertex(string v)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(v))
+                    return Add(v);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool RemoveVertex(string v)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(v))
+                    return Remove(v);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool AddEdge(string v1, string v2)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(v1) && !string.IsNullOrEmpty(v2))
+                    return Add(v1, v2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool AddEdge(string v1, string v2, int weight)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(v1) && !string.IsNullOrEmpty(v2))
+                    return Add(v1, v2, weight);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool RemoveEdge(string v1, string v2)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(v1) && !string.IsNullOrEmpty(v2))
+                    return Remove(v1, v2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        private bool Remove(string v)
+        {
+            return vertices.Remove(v);
+        }
+
+        private bool Remove(string v1, string v2)
+        {
+            var row = vertices[v1];
+            var column = vertices[v2];
+
+            matrix[row, column] = 0;
+
+            return true;
+        }
+
+        private bool Add(string v)
+        {
+            bool added = false;
+
+            if (!vertices.ContainsKey(v))
+            {
+                if (vertices.Keys.Count < matrix.GetLength(0))
+                {
+                    vertices[v] = getEmptyIndex(matrix);
+                    added = true;
+                }
+            }
+
+            return added;
+        }
+
+        private bool Add(string v1, string v2, int weight = 0)
+        {
+            var row = vertices[v1];
+            var column = vertices[v2];
+
+            matrix[row, column] = weight;
+
+            return true;
+        }
+
+        private int getEmptyIndex(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                bool hasEdge = false;
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0)
+                        hasEdge = true;
+                }
+
+                if (!hasEdge && !vertices.ContainsValue(i))
+                    return i;
+            }
+
+            return -1;
+        }
     }
 }
